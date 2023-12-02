13353e65cc276843106cbab5380a8ea6c7fc1396
Rasmus Svanberg <rasmus.svahnberg97@gmail.com>
2023-12-02 13:38:50 +0100

    Day 2 done

--- src/02/02_2_1.js
+++ src/02/02_2_1.js
@@ -5,27 +5,33 @@ import fs from 'fs';
 export default function calculateMinimumSetOfCubes() {
   const lines = fs.readFileSync('./src/02/input/02.txt', 'utf-8').trim().split('\n');
 
+  const powerPerGame = [];
+
   lines.forEach((line) => {
     const game = line.match(/Game (\d+): (.+)/);
-    // const gameID = parseInt(game[1], 10);
+    const gameID = parseInt(game[1], 10);
 
     const gameSubsets = game[2].split(';');
 
-    let gameIsPossible = true;
+    const minAmounts = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
 
     gameSubsets.forEach((subset) => {
       const counts = subset.split(',').map((count) => count.trim().split(' '));
 
-      let minAmountOfRed = 0;
-      let minAmountOfGreen = 0;
-      let minAmountOfBlue = 0;
-
-      return counts.forEach(([amount, color]) => {
-        // Calculate minimum of each color
-        return true;
+      counts.forEach(([amount, color]) => {
+        if (amount > minAmounts[color]) {
+          minAmounts[color] = parseInt(amount, 10);
+        }
       });
     });
 
+    console.log(`MIN AMOUNT GAME ${gameID}: `, minAmounts);
+    powerPerGame.push(minAmounts.red * minAmounts.green * minAmounts.blue);
   });
 
+  console.log('Total sum of powers: ', powerPerGame.reduce((sum, value) => sum + value));
 }
