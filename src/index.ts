a2b6854b0ed7d64d31de2f60b6d81f8035ba6818
Rasmus Svanberg <rasmus.svahnberg97@gmail.com>
2023-12-05 15:16:31 +0100

    Moved puzzles to own folder

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // Day 1
-import { parseInputFromFile1 } from './01_1_1'
-import { parseInputFromFile2 } from './01_2_1'
+import { parseInputFromFile1 } from './puzzles/01_1_1'
+import { parseInputFromFile2 } from './puzzles/01_2_1'
 
 // Day 2
-import calculatePossibleGames from './02_1_1'
-import calculateMinimumSetOfCubes from './02_2_1'
+import calculatePossibleGames from './puzzles/02_1_1'
+import calculateMinimumSetOfCubes from './puzzles/02_2_1'
 
 // Day 3
-import getSumOfEngineParts from './3_1_1'
-import calculateGearRatios from './3_2_1'
+import getSumOfEngineParts from './puzzles/3_1_1'
+import calculateGearRatios from './puzzles/3_2_1'
 
 // Day 4
-import calculateTotalPointsInTickets from './4_1_1'
-import calculateTotalAmountOfScratchcards from './4_2_1'
+import calculateTotalPointsInTickets from './puzzles/4_1_1'
+import calculateTotalAmountOfScratchcards from './puzzles/4_2_1'
 
 // Day 5
-import findClosestLocationNumber from './5_1_1'
-import something from './5_2_1'
+import findClosestLocationNumber from './puzzles/5_1_1'
+import something from './puzzles/5_2_1'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function Run () {
